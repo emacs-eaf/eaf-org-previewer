bd9d9cd50de1b64bdbc0419df9c8746110de29a0
Andy Stewart <lazycat.manatee@gmail.com>
2023-02-26 22:34:17 +0800

    Remove unused import.

--- buffer.py
+++ buffer.py
@@ -19,11 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt6 import QtCore
 from PyQt6.QtCore import QUrl
-from PyQt6.QtGui import QColor
 from core.webengine import BrowserBuffer
-from core.utils import get_emacs_var, get_app_dark_mode
+from core.utils import get_app_dark_mode
 import os
 
 class AppBuffer(BrowserBuffer):
