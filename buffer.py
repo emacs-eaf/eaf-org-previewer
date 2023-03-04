60e7edeeaa9c11a610626b9573260d9508e860b1
Andy Stewart <lazycat.manatee@gmail.com>
2023-03-04 11:18:42 +0800

    Use super().update_theme to remove duplicate code.

--- buffer.py
+++ buffer.py
@@ -38,11 +38,8 @@ class AppBuffer(BrowserBuffer):
 
     @interactive
     def update_theme(self):
+        super().update_theme()
         self.dark_mode = get_app_dark_mode("eaf-org-dark-mode")
-        self.theme_foreground_color = get_emacs_theme_foreground()
-        self.theme_background_color = get_emacs_theme_background()
-        self.buffer_widget.eval_js("document.body.style.background = '{}'; document.body.style.color = '{}'".format(
-            self.theme_background_color, self.theme_foreground_color))
 
         self.load_org_html_file()
         self.buffer_widget.reload()
